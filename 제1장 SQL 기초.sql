143f1615f9f6659628faa702271287e15fc21091
bskim617 <bskim617@gmail.com>
2022-08-22 13:36:47 +0900

    제1장 1-6 실습완료

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -18,13 +18,13 @@ DROP TABLE `User1`;
 
 #실습 1-3
 INSERT INTO `User1` VALUES ('A101', '김유신', '010-1234-1111', 25);
-INSERT INTO `User1` VALUES ('A102', '김춘추', '010-2222-1234', 28);
-INSERT INTO `User1` VALUES ('A103', '장보고', '010-3131-2424', 30);
+INSERT INTO `User1` VALUES ('A102', '김춘추', '010-2222-2222', 23);
+INSERT INTO `User1` VALUES ('A103', '장보고', '010-3131-3333', 32);
 INSERT INTO `User1` (`uid`, `name`, `age`) VALUES ('A104', '강감찬', '45');
 INSERT INTO `User1` SET
 						`uid`='A105',
                         `name`='이순신',
-                        `hp`='010-3333-4444';
+                        `hp`='010-1234-5555';
                         
 
 #실습 1-4
@@ -37,14 +37,14 @@ SELECT `uid`, `name`, `age` FROM `User1`;
 
 #실습 1-5
 UPDATE `User1` SET `hp`='010-1234-4444' WHERE `uid`='A104';
-UPDATE `User1` SET `age`=51  WHERE `uid`='A101';
-UPDATE `Uswe1` SET 
-					`hp`='010-1234-1001', 
-					`age`=27 
-				WHERE 
-					`uid`= 'A101';
+UPDATE `User1` SET `age`=51  WHERE `uid`='A105';
+UPDATE `User1` SET `hp`='010-1234-1001', `age`=27  WHERE `uid`= 'A101';
                     
 #실습 1-6
+delete from `User1` where `uid`='A101';
+delete from	`User1`	where `uid`='A102' AND `age`=25;
+delete from `User1` where `age` >= 30;
+
 #실습 1-7
 #실습 1-8
 #실습 1-9
