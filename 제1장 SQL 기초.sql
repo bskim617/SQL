fd3abb6e58f0a2d63828fa36b5b36e055a0a9151
bskim617 <bskim617@gmail.com>
2022-08-22 13:51:31 +0900

    1-6실습 완료

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -21,8 +21,7 @@ INSERT INTO `User1` VALUES ('A101', '김유신', '010-1234-1111', 25);
 INSERT INTO `User1` VALUES ('A102', '김춘추', '010-2222-2222', 23);
 INSERT INTO `User1` VALUES ('A103', '장보고', '010-3131-3333', 32);
 INSERT INTO `User1` (`uid`, `name`, `age`) VALUES ('A104', '강감찬', '45');
-INSERT INTO `User1` SET
-						`uid`='A105',
+INSERT INTO `User1` SET	`uid`='A105',
                         `name`='이순신',
                         `hp`='010-1234-5555';
                         
